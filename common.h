3f081ac4fdbc6b7ab5b15460168038cca62e552e
Vasu Mahesh <vasu.mahesh@hotmail.com>
2018-03-09 02:27:06 -0500

    add: cmake

--- common.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-typedef uint32_t U32;
-typedef uintptr_t UPTR;
---
+++ src/common.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdint.h>
+
+#define TOOLS_VERSION_MAJOR @EngineTools_VERSION_MAJOR@
+#define TOOLS_VERSION_MINOR @EngineTools_VERSION_MINOR@
+#define TOOLS_VERSION_PATCH @EngineTools_VERSION_PATCH@
+
+typedef uint32_t U32;
+typedef uintptr_t UPTR;
